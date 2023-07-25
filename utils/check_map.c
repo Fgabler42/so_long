345e47692ce547df2377b96e6738c87195660bde
Fritz Gabler <fgabler@1-A-2.42heilbronn.de>
2023-07-25 16:17:20 +0200

    working check_file_type

--- utils/check_map.c
+++ utils/check_map.c
@@ -6,7 +6,7 @@
 /*   By: fgabler <mail@student.42heilbronn.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/10 15:21:52 by fgabler           #+#    #+#             */
-/*   Updated: 2023/07/24 18:08:08 by fgabler          ###   ########.fr       */
+/*   Updated: 2023/07/25 16:16:21 by fgabler          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,45 +15,25 @@
 
 char    *get_input(char *PathToFile);
 void    free_allocated_array (char **array);
-int check_file_type(char *input);
+int check_file_type(char **string_of_arguments);
 
-int check_map (int av, char **ac)
+int check_map (int arguments, char **string_of_arguments)
 {
-    char    *map_input;
-	int	a;
-
-	a = av;
-
-    if (check_file_type(ac[1]))
-	{
+	if (arguments != 2)
+		return (1);
+    if (check_file_type(string_of_arguments))
 		return(1);
-	}
-	ft_printf("%d", check_file_type(ac[1]));
-    map_input = get_input(*ac);
     return (0); 
 }
 
-int check_file_type(char *input)
+int check_file_type(char **string_of_arguments)
 {
-    int i;
-    char    **store_split_path;
-    char    **store_file_type;
-
-    i = 0;
-    store_split_path = ft_split(input, '/'); 
-    
-    while(store_split_path[i] != NULL)
-        i++;
-    store_file_type = ft_split(store_split_path[i - 1], '.');
-    free_allocated_array(store_split_path);
-    i = 0;
-    while (store_file_type[1] != NULL)
-        i++;
-    if (ft_strncmp(store_file_type[i], "ber", 3)) 
-        return (1);
-    free_allocated_array(store_file_type);
-    return (0);
-    
+	int	lengsOfString;
+	
+	lengsOfString = ft_strlen(string_of_arguments[1]);
+	if(ft_strncmp((string_of_arguments[1] + (lengsOfString - 4)), ".ber", 4))
+		return (1);
+	return (0);
 }
 
 void    free_allocated_array (char **array)
